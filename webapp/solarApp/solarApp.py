9bce812179f7c1b3ef3bdacbbefaf7235538bb5a
Scott Little <scott.alan.little@gmail.com>
2015-08-13 20:14:00 -0700

    thur2 night mac

--- webapp/solarApp/solarApp.py
+++ webapp/solarApp/solarApp.py
@@ -18,12 +18,20 @@ def about_page():
 def word_counter():
 
     # get data from request form, the key is the name you set in your form
+    
     panel_type_data = request.form['panel_type']
-    number_panels_data = request.form['number_panels']
-    number_panels_data = int(number_panels_data)
+    number_panels_data = int(request.form['number_panels'])
+    month_data = int(request.form['month'])
+    hour_data = int(request.form['hour'])
+    
+    if panel_type_data == 'Solyndra':
+        predicted_power = 0
+    else:
+        predicted_power = number_panels_data * 180.0
     
     return render_template('template2.html', panel_type_data=panel_type_data, 
-                           number_panels_data=number_panels_data)
+                           number_panels_data=number_panels_data, month_data=month_data, 
+                          hour_data=hour_data, predicted_power=predicted_power)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
