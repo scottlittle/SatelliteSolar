25c9b175746fde30d34d29f98d856b70f8cc7b2f
Scott Little <scott.alan.little@gmail.com>
2015-08-14 15:04:27 -0700

    fri2 3pm webapp

---
+++ get_satellite_data.py
@@ -0,0 +1,38 @@
+# the directory where this is run must have the list.txt file that
+# generates the local http files (http_files) with the command:
+#  "wget -i list.txt" at the command line. This command should be
+# run before running this script
+
+import pandas as pd
+import os
+import glob
+import requests
+
+mypath = "data/satellite/sfarea/" #"data/satellite/colorado/summer6months/"
+
+http_files = filter(os.path.isfile, glob.glob(mypath + "*")) #local http files
+http_files.sort(key=lambda x: os.path.getmtime(x)) #sorts by time
+http_files = [os.path.basename(i) for i in http_files] #extract filename
+my_list_file = http_files[0] #pop out the list used to generate files in this dir
+http_files = http_files[1:] #keep what is not popped out
+
+with open(mypath + my_list_file) as f: #find noaa http files
+    begin_urls = f.read().splitlines() #beginning part of urls to make downloads
+
+len_begin_urls = len(begin_urls) #used for displaying completion percentage
+for i, begin_url in enumerate(begin_urls):
+
+	print "------------- ", 100.0 * i/len_begin_urls, " percent done overall---------------"
+
+	df = pd.read_html(mypath + http_files[i],header=0)[0] #read local http file into df
+
+	filenames = []
+	for j in range(len(df)): #generate filenames from dataframe for data
+	    filenames.append(df.loc[j, 'Name'])
+
+	for filename in filenames:
+		req = requests.get(begin_url + '/' + filename)
+		with open( mypath + 'data/' + filename , 'wb' ) as fout: #save data!
+			fout.write(req.content)
+
+
--- webapp/solarApp/solarApp.py
+++ webapp/solarApp/solarApp.py
@@ -22,16 +22,41 @@ def word_counter():
     panel_type_data = request.form['panel_type']
     number_panels_data = int(request.form['number_panels'])
     month_data = int(request.form['month'])
+    day_data = int(request.form['day'])
     hour_data = int(request.form['hour'])
     
     if panel_type_data == 'Solyndra':
         predicted_power = 0
     else:
         predicted_power = number_panels_data * 180.0
-    
+
+    from datetime import datetime,timedelta, time
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from data_helper_functions_webapp import find_filename, return_satellite_data
+
+    desired_channel = 'BAND_01'
+    desired_date = datetime(2014, month_data, day_data)
+    desired_timedelta = timedelta(hours = hour_data)
+    desired_datetime = desired_date + desired_timedelta
+    satellite_filefolder = '../../data/satellite/colorado/summer6months/data/'
+    sensor_filefolder = '../../data/sensor_data/colorado6months/'
+    pvoutput_filefolder = '../../data/pvoutput/pvoutput6months/'
+
+    #satellite data
+    satellite_filename = find_filename(desired_datetime, desired_channel, satellite_filefolder)
+    lons, lats, data = return_satellite_data(satellite_filename, satellite_filefolder)
+
+    plt.figure(figsize=(8, 8))
+    imgplot = plt.imshow(data)
+    imgplot.set_interpolation('none')
+    plt.savefig('static/images/foo.png', bbox_inches='tight')
+
     return render_template('template2.html', panel_type_data=panel_type_data, 
                            number_panels_data=number_panels_data, month_data=month_data, 
-                          hour_data=hour_data, predicted_power=predicted_power)
+                           day_data=day_data, hour_data=hour_data, 
+                           predicted_power=predicted_power)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
