73aa7bbbf69534932eb465474e6150c2fa13ee63
Ubuntu <ubuntu@ip-172-31-59-20.ec2.internal>
2015-08-19 03:46:08 +0000

    aws webapp

--- webapp/solarApp/solarApp.py
+++ webapp/solarApp/solarApp.py
@@ -32,4 +32,4 @@ def output():
                            predicted_power=int(predicted_power))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
